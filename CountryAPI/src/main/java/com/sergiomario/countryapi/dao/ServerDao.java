fb72145c30b7c8f570602af7d7519aa3ea4d6599
margb7 <mariogonzalezgoian@gmail.com>
2022-11-05 14:50:17 +0100

    Librería de JDBC

--- CountryAPI/src/main/java/com/sergiomario/countryapi/dao/ServerDao.java
+++ CountryAPI/src/main/java/com/sergiomario/countryapi/dao/ServerDao.java
@@ -2,6 +2,7 @@ package com.sergiomario.countryapi.dao;
 
 import com.sergiomario.countryapi.model.country.Country;
 
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ServerDao {
@@ -10,6 +11,7 @@ public class ServerDao {
 
     private final String BBDD_USER = "";
     private final String BBDD_PWD = "";
+    private Connection db;
 
     static {
 
@@ -19,15 +21,40 @@ public class ServerDao {
 
     private ServerDao() {
 
+        try {
 
+            db = DriverManager.getConnection("", BBDD_USER, BBDD_PWD);
+
+        } catch (SQLException e) {
+
+
+
+        }
 
     }
 
     public ArrayList<Country> searchByName(String searchStr) {
 
         ArrayList<Country> countries = new ArrayList<>();
+        try {
 
-        // consulta
+            PreparedStatement ps = db.prepareStatement("SELECT * FROM PAISES WHERE NOMBRE = ?");
+            ResultSet rs;
+
+            ps.setString(1, searchStr);
+
+            rs = ps.executeQuery();
+
+            while (rs.next() ) {
+
+                // Parsear string de pais
+
+            }
+
+        } catch (SQLException e) {
+
+        }
+
 
         return countries;
     }
